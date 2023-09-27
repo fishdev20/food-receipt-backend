c2836773bdac084cbe3f28a0a61719380e18c35f
Marcus <minh.nguyen@triona.fi>
2023-09-27 13:22:21 +0300

    implement Authentication and Authorisation

---
+++ FoodReceipt/src/main/java/com/example/receipt/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.example.receipt.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.receipt.config.JwtService;
+import com.example.receipt.user.Role;
+import com.example.receipt.user.User;
+import com.example.receipt.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()));
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+}
---
+++ FoodReceipt/src/main/java/com/example/receipt/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.receipt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
